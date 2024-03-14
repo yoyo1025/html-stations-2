24ae8c37f6de1df01516b5b4458d98d108f51402
yoyo1025 <hsjejxys@gmail.com>
2024-03-14 17:32:56 +0900

    初級編 station13 クリア

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,11 @@
+const { isObjectBindingPattern } = require("typescript");
+
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  var textElement = document.getElementById("text");
+  if (document.getElementById("check").checked) {
+    textElement.style.backgroundColor = "red";
+  }else {
+    textElement.style.backgroundColor = "";
+  }
 }
